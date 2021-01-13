73d576828a2ecacf7e8ca7840ebc1caba5514cba
amita-shukla <amitashukla0906@gmail.com>
2021-01-13 17:56:14 +0530

    remove quotes from json values

--- blogger_source/create_blogger_posts.sh
+++ blogger_source/create_blogger_posts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir "blogger_posts"
+posts_dir="blogger_posts"
+mkdir $posts_dir
 json_file_name="blogger_posts.json"
 echo "reading ${json_file_name}..."
 readarray -t slugs < <(cat ${json_file_name} | jq .allBloggerPost.edges[].node.slug)
@@ -15,17 +16,23 @@ for((i=0; i<${postCount}; i++));
 do
   title=${titles[$i]}
   echo "reading post ${i}: ${title}"
-  slug=${slugs[$i]}
-  date=${dates[$i]}
-  markdown_body=${markdownBodies[$i]}
+  slug=$(trim_quotes ${slugs[$i]})
+  date=$(trim_quotes ${dates[$i]})
+  markdown_body=$(trim_quotes ${markdownBodies[$i]})
   dir_name=${date}--${slug}
   file_name="index.md"
   tags=${tags[$i]}
   join_tags=$(printf ",\"%s" "${tags[@]}")
   file_content="---\ntitle:${title}\ntags:[${join_tags}]\nauthor: Amita Shukla---\n\n${markdown_body}"
-  file_path=${dir_name}/${file_name}
-  mkdir ${dir_name}
+  file_path=${posts_dir}/${dir_name}/${file_name}
+  mkdir ${posts_dir}/${dir_name}
   echo "adding content to file ${file_path}"
   echo -e ${file_content} > ${file_path}
 done
 echo "all files processed successfully"
+
+trim_quotes() {
+  opt=$1
+  str=$(sed -e 's/^"//' -e 's/"$//' <<<"$opt")
+  echo $str
+}
